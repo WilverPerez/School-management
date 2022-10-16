1426d24b4cee3c407d07dd372d2d832a54c6adee
Wilver Perez <wilverp.romero@hotmail.com>
2022-10-16 19:01:07 -0400

    comment filter

--- School-management/frontend/school-management/src/app/shared/shared.module.ts
+++ School-management/frontend/school-management/src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CardComponent } from './card/card.component';
@@ -17,7 +18,9 @@ import { TableComponent } from './table/table.component';
   imports: [
     NgbModule,
     FontAwesomeModule,
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   exports: [
     SidebarComponent,
--- School-management/frontend/school-management/src/app/shared/table/table.component.ts
+++ School-management/frontend/school-management/src/app/shared/table/table.component.ts
@@ -11,18 +11,32 @@ export class TableComponent implements OnInit {
   private _totalRows = 0;
   private _maxPerPage = 10;
   pages:Array<number> = [];
-
+  search: string = '';
   @Input() configuration!: TableConfiguration<any>;
-
+  data: any;
   constructor() {}
 
   ngOnInit(): void { 
     if(!this.configuration) return;
-
+    this.data = this.configuration.data;
     this._totalRows = this.configuration.data.length;
     const page = Math.floor(this._totalRows / this._maxPerPage);
     const cantPages = page < 1 ? 1 : page;
     
     this.pages = Array(cantPages).fill(1).map((x, i) => i+1);
+  }
+
+  filter() {
+    // this.data = this.configuration.data.filter(row: Course => {
+    //   const eso = (row as string).toString().includes(this.search);
+      
+    //   return (row as string).toString().indexOf(this.search) > -1;
+    // })
+
+    // this.data = this.configuration.data.filter(row => {
+    //   const eso = (row as string).toString().includes(this.search);
+      
+    //   return (row as string).toString().indexOf(this.search) > -1;
+    // })
   }
 }
